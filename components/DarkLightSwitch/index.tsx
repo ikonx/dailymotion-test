1b9b27055cdbb259562f00439e0c6a8b21ea3448
salah <salah.lamkadem@gmail.com>
2024-06-10 23:25:13 +0200

    :sparkles: (ThemeSwitcher) create DarkLightSwitch component

---
+++ components/DarkLightSwitch/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+
+type Mode = "dark" | "light";
+
+const DarkLightSwitch = () => {
+  const [mode, setMode] = React.useState<Mode>("dark");
+
+  const onClick = (mode: Mode) => () => {
+    setMode(mode);
+  };
+
+  React.useEffect(() => {
+    const mode = localStorage.getItem("mode");
+    if (mode) {
+      setMode(mode as Mode);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    localStorage.setItem("mode", mode);
+    // TODO: retreive default CSS var instead of hardcoding (--defailt-...)
+    document.documentElement.style.setProperty(
+      "--background-rgb",
+      mode === "dark" ? "11, 11, 11" : "255, 255, 255"
+    );
+    document.documentElement.style.setProperty(
+      "--foreground-rgb",
+      mode === "dark" ? "255, 255, 255" : "11, 11, 11"
+    );
+  }, [mode]);
+
+  return (
+    <div className="flex items-center px-2 py-2 rounded-full border-solid border-2 border-[--foreground]">
+      <p>{mode}</p>
+      <button
+        className={`w-6 h-6 rounded-full ${
+          mode === "light" && "bg-[--foreground] text-[--background]"
+        }`}
+        onClick={onClick("light")}
+      >
+        l
+      </button>
+      <button
+        className={`w-6 h-6 rounded-full ${
+          mode === "dark" && "bg-[--foreground] text-[--background]"
+        }`}
+        onClick={onClick("dark")}
+      >
+        d
+      </button>
+    </div>
+  );
+};
+
+export default DarkLightSwitch;
