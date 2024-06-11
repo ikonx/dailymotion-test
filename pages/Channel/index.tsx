21abf4cc1f3dd2bae6ccf0ba1567a17e705515ec
salah <salah.lamkadem@gmail.com>
2024-06-11 13:36:16 +0200

    :sparkles: (Channel page)

---
+++ pages/Channel/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+type Props = {
+  channel: string;
+};
+
+const Channel = ({ channel }: Props) => {
+  return (
+    <div className="px-4 flex flex-col">
+    </div>
+  );
+};
+
+export default Channel;
