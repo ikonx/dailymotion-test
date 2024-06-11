6131df159ec2fdabad6a1c8123bcecc41ef1e79f
salah <salah.lamkadem@gmail.com>
2024-06-11 13:37:57 +0200

    :art: Player

---
+++ components/Player/index.tsx
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+import Script from "next/script";
+
+type Props = {
+  playerId: string;
+  videoId?: string;
+  playlistId?: string;
+  onVideoEnd?: () => void;
+};
+
+declare global {
+  interface Window {
+    dailymotion: any;
+  }
+}
+
+const Player = ({ playerId, videoId, onVideoEnd }: Props) => {
+  useEffect(() => {
+    if (window.dailymotion === undefined) {
+      window.dailymotion = {
+        onScriptLoaded: () => {
+          window.dailymotion.createPlayer("dailymotion-player", {
+            video: videoId,
+            params: {
+              autoplay: true,
+              mute: false,
+              controls: false,
+              origin: window.location.origin,
+            },
+          });
+
+          window.dailymotion
+            .getPlayer("dailymotion-player")
+            .then((player: any) => {
+              player.on(window.dailymotion.events.PLAYER_END, () => {
+                onVideoEnd?.();
+              });
+            });
+        },
+      };
+    } else {
+      if (window.dailymotion.getPlayer) {
+        window.dailymotion.createPlayer("dailymotion-player", {
+          video: videoId,
+          params: {
+            autoplay: true,
+            mute: false,
+            controls: false,
+            origin: window.location.origin,
+          },
+        });
+      }
+    }
+
+    console.log(window.dailymotion);
+
+    // return () => {
+    //   if (window.dailymotion.getPlayer) {
+    //     window.dailymotion
+    //       .getPlayer("dailymotion-player")
+    //       .then((player: any) => {
+    //         player.off(window.dailymotion.events.PLAYER_END);
+    //         player.destroy();
+    //       });
+    //   }
+    // };
+  }, []);
+
+  const getPlayerScriptSrc = () => {
+    let src = `https://geo.dailymotion.com/libs/player/${playerId}.js`;
+    if (videoId) src += `?video=${videoId}`;
+    return src;
+  };
+
+  return (
+    <>
+      <Script src={getPlayerScriptSrc()} strategy="lazyOnload" />
+      <div id="dailymotion-player"></div>
+    </>
+  );
+};
+
+export default Player;
