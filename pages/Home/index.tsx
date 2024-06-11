1545f3bf5a8f8082b69c3abcb31c82d76cf6db3e
salah <salah.lamkadem@gmail.com>
2024-06-11 13:33:39 +0200

    :sparkles: (Home page)

---
+++ pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import SearchInput from "@UI/SearchInput";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-full flex items-center justify-center text-4xl font-bold">
+      <SearchInput />
+    </div>
+  );
+};
+
+export default Home;
